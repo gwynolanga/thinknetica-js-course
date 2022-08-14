06b724de19f484908d771d5174c8bf63d0955e36
Kirill Ilyin <cuurjol@gmail.com>
2022-08-14 19:50:27 +0300

    Release Lesson 8, task 1

---
+++ lessons/lesson-8/tasks/task-1.js
@@ -0,0 +1,101 @@
+/**
+ * Написать собственную простую реализацию, которая будет соответствовать следующим требованиям:
+ * 1. Промис создаётся конструктором в состоянии pending
+ * 2. Промис может быть переведён в состояние fulfilled или rejected необратимо,
+ * 		т.е. промис который поменял состояние один раз, не может менять его дальше
+ * 3. Можно добавить новые обработчики в уже исполненный промис
+ * 4. Можно создать уже выполненный промис с помощью CustomPromise.resolve/reject
+ *
+ * Важный нюанс: Промис должен работать асинхронно, для реализации этого можно использовать
+ * setTimeout c нулевой задержкой
+ */
+
+class CustomPromise {
+	constructor(executor) {
+		this.#state = 'pending'
+		this.#result = undefined
+		this.#thenCallBacks = []
+		this.#catchCallBacks = []
+
+		try {
+			executor(this.#resolve.bind(this), this.#reject.bind(this))
+		} catch (error) {
+			this.#reject(error)
+		}
+	}
+
+	static resolve(value) {
+		return new CustomPromise((resolve) => { resolve(value) })
+	}
+
+	static reject(error) {
+		return new CustomPromise((_, reject) => { reject(error) })
+	}
+
+	then(thenCallback, catchCallback) {
+		return new CustomPromise((resolve, reject) => {
+			if (this.#state === 'pending') {
+				if (thenCallback) {
+					this.#thenCallBacks.push(() => {
+						try {
+							resolve(thenCallback(this.#result))
+						} catch (error) {
+							reject(error)
+						}
+					})
+				}
+
+				if (catchCallback) {
+					this.#catchCallBacks.push(() => {
+						try {
+							reject(catchCallback(this.#result))
+						} catch (error) {
+							reject(error)
+						}
+					})
+				}
+			}
+
+			if (thenCallback && this.#state === 'fulfilled') {
+				try {
+					resolve(thenCallback(this.#result))
+				} catch (error) {
+					reject(error)
+				}
+			}
+
+			if (catchCallback && this.#state === 'rejected') {
+				try {
+					reject(catchCallback(this.#result))
+				} catch (error) {
+					reject(error)
+				}
+			}
+		})
+	}
+
+	catch(callback) {
+		return this.then(null, callback)
+	}
+
+	#state
+	#result
+	#thenCallBacks
+	#catchCallBacks
+
+	#resolve(value) {
+		if (this.#state === 'pending') {
+			this.#state = 'fulfilled'
+			this.#result = value
+			this.#thenCallBacks.forEach((callback) => callback(value))
+		}
+	}
+
+	#reject(error) {
+		if (this.#state === 'pending') {
+			this.#state = 'rejected'
+			this.#result = error
+			this.#catchCallBacks.forEach((callback) => callback(error))
+		}
+	}
+}
